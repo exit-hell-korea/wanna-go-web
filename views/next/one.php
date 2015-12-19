afd6345bf58f0c51447ee8ddaecb54aee267dbd1
dolsup <1890mah@gmail.com>
2015-12-19 21:26:06 +0900

    add img assets / one.php

--- views/next/one.php
+++ views/next/one.php
@@ -10,9 +10,39 @@
   </div>
   <div id="con">
     <div class="container">
-      <div class="row">
-        <!--S:Contemts -->
-        <p class="text-center d_description" style="margin-top:20%">
+      <div class="row" id="selectpage">
+        <!--S:Contents -->
+        <div class="imgs">
+            <div id="logotitle">
+                <img src="/asset/logotitle.png">
+            </div>
+            <a href="/next?num=2" class="imgbutton">
+                <img src="/asset/gotext.png">
+            </a>
+            <a href="#" class="imgbutton disabled">
+                <img src="/asset/gocall.png">
+            </a>
+        </div>
+        <style>
+            #selectpage {
+                background-color: #D4D2BE;
+            }
+            .disabled {
+                cursor: disabled;
+                filter: grayscale(30%);
+            }
+            .imgs {
+                position: absolute;
+                bottom: 0;
+            }
+            .imgbutton img {
+                width: 100%;
+                height: auto;
+            }
+        </style>
+        
+        
+        <!-- <p class="text-center d_description" style="margin-top:20%">
           <span style="font-size:1.2em">100%</span>퇴근 가능한<br>
           절적한 이유를 제공해주는 치밀한<br>
           <span>"퇴근 핑계 제공 서비스"</span>
@@ -21,8 +51,8 @@
           <div class="red-button text-center">
             <span class="out_hell" onclick="location.href='/next?num=0'">탈출하기</span>
           </div>
-        </p>
-        <!--E:Contemts -->
+        </p> -->
+        <!--E:Contents -->
       </div>
     </div>
   </div>
